d321ab9db867f96f55accd9be24b5488ee7501ea
sirdomestos <135537200+sirdomestos@users.noreply.github.com>
2023-07-05 15:12:09 +0000

    new

--- hello_app/app.py
+++ hello_app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'My server IP address is {os.environ["HOST_IP"]}'
+    return f'05.07.2023'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
